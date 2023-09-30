83ebe8202cb5b270416f3d6b7f4b2364a07dbcc9
raehan <raehan900809@gmail.com>
2023-10-01 00:10:10 +0900

    add: layout, page of app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
-import './globals.css'
+// import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import {PropsWithChildren} from "react";
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -11,12 +9,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   )
 }
